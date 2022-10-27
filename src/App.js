d674fd4d47165bcf92cd73917ed3914b7ae0cc6e
MMoustafa200 <m.moustafa.m21@gmail.com>
2022-10-27 15:42:52 +0200

    css libaray

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React, { Component } from "react";
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import background from './assets/images/background.svg'
-import NavBar from "./components/navBar/NavBar";
-import SideBar from "./components/sideBar/SideBar";
-import Home from "./components/homePage/Home";
-import './assets/styles/bg.css';
-import './assets/styles/mainStyles.css';
-import Tools from "./components/ToolsIcon/Tools";
+
+// main style lib, ( global to all the components ).
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="full-width full-height">
-          <img src={background} alt="ProjectBackground" className="bg absolute full-height fixed" />
-          <SideBar />
-          <NavBar />
-          <Tools />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <div className="full-width flex-col-center gray-bg shadow-5px">
+        <p className="open-sans yellow">Hello World</p>
+        <p className="inter yellow">Hello World</p>
+        <p className="pt-sans yellow">Hello World</p>
+      </div>
     );
   }
 }
