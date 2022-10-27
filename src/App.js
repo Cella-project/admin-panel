790aafe355c5c99c282d07e274d7dd29e147e854
MMoustafa200 <m.moustafa.m21@gmail.com>
2022-10-27 18:16:12 +0200

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
 
-// main style lib, ( global to all the components ).
+import NavBar from './components/global/NavBar';
 
 class App extends Component {
   render() {
     return (
-      <div className="full-width flex-col-center gray-bg shadow-5px">
-        <p className="open-sans yellow">Hello World</p>
-        <p className="inter yellow">Hello World</p>
-        <p className="pt-sans yellow">Hello World</p>
-      </div>
+      <NavBar />
     );
   }
 }
