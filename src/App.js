9e7fb67344369c5bdef6b071836b59b7087a0c20
Abdelrahman El Sharabasy <115502500+sharabasy@users.noreply.github.com>
2022-11-12 18:42:18 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import React, { Component } from "react";
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import background from './assets/images/background.svg'
-import NavBar from "./components/navBar/NavBar";
-import SideBar from "./components/sideBar/SideBar";
-import Home from "./components/homePage/Home";
-import './assets/styles/bg.css';
-import './assets/styles/mainStyles.css';
-import Tools from "./components/ToolsIcon/Tools";
+import React, { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import background from './assets/images/background.svg';
+import NavBar from "./components/global/navBar/NavBar";
+import SideBar from "./components/global/sideBar/SideBar";
+import MobMenu from './components/global/mobMenu/MobMenu';
+import Tools from "./components/global/ToolsIcon/Tools";
+import Routing from "./router/Routing";
+import './assets/styles/bg.scss';
+import './assets/styles/style.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="full-width full-height">
-          <img src={background} alt="ProjectBackground" className="bg absolute full-height fixed" />
-          <SideBar />
-          <NavBar />
-          <Tools />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            
-          </Routes>
-        </div>
-      </BrowserRouter>
-    );
-  }
+const App = () => {
+
+  const [mobMenuShown, setMobMenuShown] = useState(false);
+
+  return (
+    <BrowserRouter>
+      <div className="full-width full-screen-height app">
+        <img src={background} alt="ProjectBackground" className="bg full-screen-height" />
+        <SideBar />
+        {mobMenuShown && <MobMenu menuToggle={setMobMenuShown} />}
+        <NavBar menuToggle={setMobMenuShown} />
+        <Tools />
+        <Routing />
+        
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
