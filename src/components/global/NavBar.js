f0a7a4dc8d665f626e7a839922ddfc3e2f2d34c8
Abdelrahman El Sharabasy <boodysharabas@gmail.com>
2023-06-24 18:18:07 +0300

    routes

--- src/components/global/NavBar.js
+++ src/components/global/NavBar.js
@@ -32,7 +32,7 @@ const NavBar = ({ menuToggle }) => {
     return (
         <div className={`nav-bar ${mode === 'dark-mode' ? 'dark' : ''} full-width`} style={{ marginTop: (userData.validEmail !== null && userData.validEmail) ? '0px' : '15px' }}>
             <div className='nav-bar--cont full-width white-bg flex-row-between'>
-                <Link to={'/'} className='home-link'>
+                <Link to={'/admin-panel'} className='home-link'>
                     <img src={mode === 'dark-mode' ? darkLogo : logo} alt='ACTORE' className='nav-bar--logo pointer' />
                 </Link>
                 <i className={`nav-bar--btn bi bi-list ${mode === 'dark-mode' ? 'gray' : 'orange'} size-38px pointer`} onClick={handleClick}></i>
