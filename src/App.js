1e8a5b139824816b553d087d9462407bec71f1f6
Abdelrahman El Sharabasy <boodysharabas@gmail.com>
2023-07-05 21:25:26 +0300

    notification read all

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import StickyBoard from './components/sticky/StickyBoard';
 import Popup from './components/popups/Popup';
-import { adminActions, authActions, notificationActions } from "./apis/actions";
+import { adminActions, authActions } from "./apis/actions";
 import { authMutations, connectedUsersMutations, adminMutations, driverMutations } from "./redux/mutations";
 
 import router from "./router/router";
@@ -50,10 +50,7 @@ const App = () => {
         userData: user,
         access: accessToken,
         refresh: refreshToken
-      }));
-
-      dispatch(notificationActions.getAllNotifications(0));
-      
+      }));      
       socket.auth = { token: accessToken };
       socket.connect();
 
